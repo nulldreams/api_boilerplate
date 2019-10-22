82fcd760367273e21b4aef63748ef8d38697ac00
Heitor Dobeis <heitordobeis@gruposgapetro.com.br>
2019-10-22 14:02:10 -0300

    fix: body-parser and .env.example abstraction

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const routes = require('./app/api/routes')
-const bodyParser = require('body-parser')
 const morgan = require('./lib/morgan')
 const app = express()
 
 app.use(morgan)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 routes(app)
 
